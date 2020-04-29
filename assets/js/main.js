46be95ff36a8530dc8321f22e34420df30e68963
Hazar <hazar.cakir.cmpe@gmail.com>
2020-04-29 11:18:29 +0300

    Photos updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -163,7 +163,7 @@
   $(window).on('load', function() {
     var portfolioIsotope = $('.portfolio-container').isotope({
       itemSelector: '.portfolio-item',
-      layoutMode: 'fitRows'
+      layoutMode: 'fitRows',
     });
 
     $('#portfolio-flters li').on('click', function() {
